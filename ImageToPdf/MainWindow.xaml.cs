81592a7815828b5b34746f51341b2fb09a8a7b93
David Shnayder <dusrdev@gmail.com>
2022-09-10 13:13:34 +0300

    -

--- ImageToPdf/MainWindow.xaml.cs
+++ ImageToPdf/MainWindow.xaml.cs
@@ -9,4 +9,12 @@ public partial class MainWindow : Window {
 
     private void BorderDrop_Drop(object sender, DragEventArgs e) {
     }
+
+    private void BtnConvert_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void LstFiles_Drop(object sender, DragEventArgs e) {
+
+    }
 }
