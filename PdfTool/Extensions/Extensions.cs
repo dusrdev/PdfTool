35c6ccd6ab61b5d88c5c8c03b57df8424bfb22fe
David Shnayder <dusrdev@gmail.com>
2022-09-17 12:08:16 +0300

    Added a function that executes a collection of func's simultaneously

---
+++ PdfTool/Extensions/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+
+namespace PdfTool.Extensions;
+
+internal static class Extensions {
+    /// <summary>
+    /// Parallelizes func execution
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source">Collection of func's</param>
+    /// <param name="degreeOfParallelization">essentially a batch count, if set to -1 it will be the amount of cpu cores</param>
+    /// <param name="body"></param>
+    /// <returns></returns>
+    internal static Task ParallelForEachAsync<T>(this IEnumerable<T> source, int degreeOfParallelization, Func<T, Task> body) {
+        if (degreeOfParallelization == -1) {
+            degreeOfParallelization = Environment.ProcessorCount;
+        }
+
+        async Task AwaitPartition(IEnumerator<T> partition) {
+            using (partition) {
+                while (partition.MoveNext()) {
+                    await body(partition.Current);
+                }
+            }
+        }
+
+        return Task.WhenAll(Partitioner
+            .Create(source)
+            .GetPartitions(degreeOfParallelization)
+            .AsParallel()
+            .Select(AwaitPartition));
+    }
+}
