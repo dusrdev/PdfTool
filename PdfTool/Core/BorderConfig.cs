d329d903bf705d154b7b6102bf39e442b4795cb0
David Shnayder <dusrdev@outlook.com>
2023-04-15 11:44:22 +0300

    Added data types to hold different styles for borders

---
+++ PdfTool/Core/BorderConfig.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Media;
+
+namespace PdfTool.Core;
+
+public readonly record struct BorderConfig {
+    public readonly SolidColorBrush StaticColor { get; init; }
+	public readonly SolidColorBrush ActiveColor { get; init; }
+}
---
+++ PdfTool/Core/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Media;
+
+namespace PdfTool.Core;
+
+internal static class Constants {
+    public static readonly Dictionary<string, BorderConfig> BorderConfigs = new() {
+        {
+            "ConvertBorder", new BorderConfig {
+                StaticColor = Brushes.Lime,
+                ActiveColor = Brushes.GreenYellow
+            }
+        },
+        {
+            "MergeBorder", new BorderConfig {
+                StaticColor = Brushes.DodgerBlue,
+                ActiveColor = Brushes.LightBlue
+            }
+        },
+        {
+            "SplitBorder", new BorderConfig {
+                StaticColor = Brushes.Crimson,
+                ActiveColor = Brushes.OrangeRed
+            }
+        }
+    };
+}
